37fc9d892f3aa6e00a551aa6b7162813804dedc2
Erwin de Keijzer <erwin@gnur.nl>
2017-04-28 14:36:15 +0200

    W.I.P.

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/asdine/storm"
+	"github.com/google/uuid"
+)
+
+type Show struct {
+	ID            uuid.UUID
+	Name          string `storm:"unique"`
+	Regexp        string
+	Active        bool `storm:"index"`
+	Episodeidtype string
+	Minimal       int
+}
+
+type Episode struct {
+	ID          uuid.UUID
+	Title       string
+	Showid      uuid.UUID `storm:"index"`
+	Added       time.Time `storm:"index"`
+	Magnetlink  string    `storm:"unique"`
+	Episodeid   string
+	Downloaded  bool `storm:"index"`
+	Downloading bool `storm:"index"`
+}
+
+type Event struct {
+	ID        uuid.UUID
+	Timestamp time.Time `storm:"index"`
+	Related   []string  `storm:"index"`
+	Data      string
+}
+
+func main() {
+	db, err := storm.Open("test.db")
+	defer db.Close()
+	u1, _ := uuid.NewRandom()
+	fmt.Println(u1)
+	if err != nil {
+		fmt.Println("nu al een faal", err.Error())
+	}
+	var events []Event
+	err = db.All(&events)
+	for _, e := range events {
+		fmt.Println("------------------------")
+		fmt.Println(e.ID)
+		fmt.Println(e.Data)
+		fmt.Println(e.Timestamp)
+	}
+	fmt.Println("jep")
+}
