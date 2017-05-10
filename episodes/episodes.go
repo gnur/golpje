7a2033592b233d1d508f285424b5992b2d27052e
Erwin de Keijzer <erwin@gnur.nl>
2017-05-10 19:58:30 +0200

    Slight cleanup

--- episodes/episodes.go
+++ episodes/episodes.go
@@ -1,20 +1,10 @@
-package structs
+package episodes
 
 import (
 	"time"
 
 	"github.com/google/uuid"
 )
-
-// Show a show
-type Show struct {
-	ID            uuid.UUID
-	Name          string `storm:"unique"`
-	Regexp        string
-	Active        bool `storm:"index"`
-	Episodeidtype string
-	Minimal       int
-}
 
 // Episode : The basic struct for all episodes
 type Episode struct {
